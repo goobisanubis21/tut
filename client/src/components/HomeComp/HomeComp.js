ae6577ee7e616f7c222cb7789c6ec9ebd49d0515
Anthony Parrino <anthony.parrino@yahoo.com>
2021-10-26 23:49:59 -0400

    styling

--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -1,4 +1,5 @@
 import React, {useRef} from 'react';
+import './homeComp.css';
 // import API from '../../utils/API'
 
 const HomeComp = () => {
@@ -13,9 +14,12 @@ const HomeComp = () => {
     // }
 
     return (
-        <div>
-            <div>
+        <div className='home-container'>
+            <div className='home-title'>
                 <h1>Tutor Company Name</h1>
+            </div>
+            <div>
+                
             </div>
             {/* <form onSubmit={handleSubmit}>
                 <input className='first' ref={firstNameRef}></input>
